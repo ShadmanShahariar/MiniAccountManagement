8f78794388c39432bd94551999c81a1a3b0d9945
Shadmun Shahariar <109878144+ShadmanShahariar@users.noreply.github.com>
2025-07-02 10:03:02 +0600

    Give  User Role and module permission screen

---
+++ ViewModels/ModulePermissionViewModel.cs
@@ -0,0 +1,11 @@
+﻿namespace MiniAccountManagement.ViewModels
+{
+    public class ModulePermissionViewModel
+    {
+        public int ModuleId { get; set; }
+        public string ModuleName { get; set; }
+        public bool IsAllowed { get; set; }
+    }
+
+
+}
